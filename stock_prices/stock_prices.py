6afa99459ad7b39fb3ea15b8416752f45e7d3d81
Hargobind Atwal <ethaipath@gmail.com>
2019-02-20 16:09:24 -0800

    Got to solution for stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -22,23 +22,66 @@ you have to buy before you sell
 so if I walk through the list when I find a lower buy price and then if that 
 is able to find a higher profit then set those values
 
+looking at the second test case the problem is really find max profit or find least losses
+So if 
+
+set low_price and max_price to first index
+loop through prices
+when a lower buy price is found 
+loop through the rest of the list
+check if a greater profit can be found 
+if so set low_price to the new buy price and set max_price to the new sell price
+repeat until len(arr)-1
 '''
 
+'''
+[10, 7, 5, 8, 11, 9]), 6)
+[100, 90, 80, 50, 20, 10]), -10)
+[1050, 270, 1540, 3800, 2]), 3530)
+[100, 55, 4, 98, 10, 18, 90, 95, 43, 11, 47, 67, 89, 42, 49, 79]), 94)
+'''
+
+import argparse
 
 def find_max_profit(prices):
-  low_price = max_price = prices[0]
+  print('=======Start=======')
+  low_price = prices[0]
+  profit = prices[1] - low_price
 
-  for p in prices:
+  for current in prices[1:]:
+    print('profit: ', profit, 'low_price: ', low_price)
+    profit = max(current - low_price, profit)
+    low_price = min(current, low_price)
 
-    if p < low_price:
+  return profit #- low_price
+
+# print(find_max_profit([1050, 270, 1540, 3800, 2]))
+
+'''
+  for p in range(2, len(prices)-1):
+    print(p, ': ', low_price, max_price)
+    if prices[p] < low_price:
+      for i in range(p, len(prices)):
+        if max_price - low_price < prices[i] - low_price:
+          print(max_price - low_price, ' < ', prices[i] - low_price)
+          low_price = prices[p]
+          max_price = prices[i]
+'''
+  #return max_price - low_price 
+'''
+def find_max_profit(prices):
+  pass      low_price = max_price = prices[0]
+
+   for p in prices:
+
+     if p < low_price:
       low_price = p
-
-    if max_price - low_price < p - low_price:
-      max_price = p
+      if max_price - low_price < p - low_price:
+        max_price = p
     #if sell_price - p > sell_price - buy_price:
 
-  return max_price - low_price 
-
+   return max_price - low_price 
+'''
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
